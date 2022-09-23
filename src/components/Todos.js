943946131898a7f3022d3f7c6a15acde0315dfd1
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-09-23 14:35:56 +0100

    message

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,7 +4,7 @@ import Todo from './Todo'
 const Todos = ({todos, deleteTodo}) => {
   return (
     <div style={{marginTop: '20px'}}>
-        <h3  style={{textAlign: 'center'}}>Your Todos</h3>
+        <h3  style={{textAlign: 'center', marginBottom: '10px'}}>Your Todos</h3>
         {todos.length < 1 ? 'You have no todos' :
         <>{todos.map((todo)=> <Todo todo={todo} key={todo.id} deleteTodo={deleteTodo}/> )}</>}
     </div>
